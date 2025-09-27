b292aafcea600fae701885d159a9df3b96805b48
jenviard <jiv19@miami.edu>
2025-09-27 20:09:05 +0000

    fix

--- jsexamples/js/click.js
+++ jsexamples/js/click.js
@@ -5,7 +5,8 @@ let lightModeClickBox = document.getElementById("lightClick");
 lightModeClickBox.innerHTML = "<p> light mode</p>"
 lightModeClickBox.classList.add("clickLight");
 
-lightModeClickBox.addEventListener("click", function(){
+lightModeClickBox.addEventListener("click", function()
+{
     console.log("clicking light box");
     document.querySelector("body").style.backgroundColor = "white";
     document.body.style.color = "black"; 
@@ -16,20 +17,21 @@ let darkModeClickBox = document.getElementById("darkClick");
 darkModeClickBox.innerHTML = "<p> dark mode</p>"
 darkModeClickBox.classList.add("clickDark");
 
-darkModeClickBox.addEventListener("click", function(){
+darkModeClickBox.addEventListener("click", function()
+{
     console.log("clicking dark box");
     document.querySelector("body").style.backgroundColor = "black";
     document.body.style.color = "white"; 
 });
 
 let enlargePhoto = document.getElementById("photo"); 
-  enlargePhoto.addEventListener("click", function() 
-  {
+enlargePhoto.addEventListener("click", function() 
+{
     enlargePhoto.classList.toggle("enlarged");
 });
 
 let enlargePhoto2 = document.getElementById("photo2"); 
-  enlargePhoto2.addEventListener("click", function() 
-  {
-    enlargePhoto2.classList.toggle("enlarged");
+enlargePhoto2.addEventListener("click", function() 
+{
+  enlargePhoto2.classList.toggle("enlarged");
 });
